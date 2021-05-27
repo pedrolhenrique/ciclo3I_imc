4f19e5e09b557add039d3a9838e54349e14587e7
Pedro Salles <pedro.silva15@alunos.unis.edu.br>
2021-05-27 18:53:11 -0300

    Adicionando Comentarios

--- AtivCiclo3IMC/src/ativciclo3imc/AtivCiclo3IMC.java
+++ AtivCiclo3IMC/src/ativciclo3imc/AtivCiclo3IMC.java
@@ -13,6 +13,7 @@ public class AtivCiclo3IMC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /**DECLARAÇÃO DE VARIAVEIS**/
         String alt;
         String peso;
         double xalt;
@@ -21,14 +22,16 @@ public class AtivCiclo3IMC {
         double metros;
         
         alt = JOptionPane.showInputDialog("DIGITE A ALTURA EM CM: EX: 170=1.70");
-        xalt = Double.parseDouble(alt);
+        xalt = Double.parseDouble(alt);/*Converte String em double*/
         
-        metros = xalt / 100;
+        metros = xalt / 100; /*Realiza o calculo de cm para metros*/
         
         peso = JOptionPane.showInputDialog("DIGITE O PESO: ");
-        xpeso = Integer.parseInt(peso);
+        xpeso = Integer.parseInt(peso);/*Converte String em inteiro*/
         
-        total = xpeso / (metros * metros);
+        total = xpeso / (metros * metros); /*Faz o calculo do IMC*/
+        
+        /***Apresenta o resultado do calculo IMC na tela***/
         JOptionPane.showMessageDialog(null,"SEU IMC É: " + total + " Kg/m");
         
         if (total < 17) {
